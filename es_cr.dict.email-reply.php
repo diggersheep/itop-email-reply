c2bd32f2268c9d00b9470b00d082ac6ad996bb7a
a.combeau <a.combeau@unistra.fr>
2018-10-31 09:56:05 +0100

    first commit

---
+++ es_cr.dict.email-reply.php
@@ -0,0 +1,43 @@
+<?php
+// Copyright (C) 2012-2015 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/**
+ * Localized data
+ *
+ * @author      Erwan Taloc <erwan.taloc@combodo.com>
+ * @author      Romain Quetiez <romain.quetiez@combodo.com>
+ * @author      Denis Flaven <denis.flaven@combodo.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+//
+// Class: TriggerOnLogUpdate
+//
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:TriggerOnLogUpdate' => 'Disparador (cuando la bitácora es actualizada)',
+	'Class:TriggerOnLogUpdate+' => 'Disparador cuando el usuario actualiza bitácora',
+	'Class:TriggerOnLogUpdate/Attribute:target_log' => 'Código de Atributo en bitácora',
+	'Class:TriggerOnLogUpdate/Attribute:target_log+' => 'Identifica que bitácora será considerada',
+
+	'UI-emry-enable' => 'Enviar respuesta por correo-e',
+	'UI-emry-noattachment' => 'Sin Anexos',
+	'UI-emry-caselog-prompt' => 'Escriba su texto aquí',
+	'UI-emry-select-attachments' => 'Seleccione Anexo...',
+	'UI-emry-attachments-to-be-sent' => 'Los siguientes anexos serán enviados:',
+	'UI-emry-select-attachments-tooltip' => 'Click para seleccionar los anexos a enviar',
+));
+
